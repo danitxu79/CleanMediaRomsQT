b92af0195e65e5ca7d71ffd7d3dcdf7890d2214f
Danitxu79 <danitxu79@users.noreply.github.com>
2019-08-16 09:02:18 +0200

    Add files via upload

---
+++ actualizar.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 16 03:32:44 2019
+
+@author: danie
+"""
+
+from os import remove, rename
+import sys
+import os
+
+remove("CleanMediaQT.py")
+rename("CleanMediaQT.act", "CleanMediaQT.py")
+execl("python CleanMediaQT.py")
